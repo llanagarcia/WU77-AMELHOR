87e51aa58fe9651741d2676d9f0c9dcd164eaed0
llanagarcia <alanamoreira130@gmail.com>
2025-06-05 20:25:54 -0300

    Update testedeidentaçao.py

--- testedeidentaçao.py
+++ testedeidentaçao.py
@@ -1,9 +1,9 @@
 #IDENTAÇÃO
 if 5>2:
     print("eu vou ali jaja!")
-
+#essa e uma estrutura identada
 if 5>3:
     print("aqui nao e o lugar correto")
-
+#essa e uma estrutura identada
 if 10>11:
     print("estou com fome")
