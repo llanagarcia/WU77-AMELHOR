7ef2b5036fbb55378db67eb224523ad301f8e401
llanagarcia <alanamoreira130@gmail.com>
2025-06-05 20:20:17 -0300

    Update testedeidentaçao.py

--- testedeidentaçao.py
+++ testedeidentaçao.py
@@ -1,6 +1,9 @@
 #IDENTAÇÃO
-if 5>10:
+if 5>2:
     print("eu vou ali jaja!")
 
+if 5>3:
+    print("aqui nao e o lugar correto")
 
-print("aqui nao e o lugar correto!")
+if 10>11:
+    print("estou com fome")
